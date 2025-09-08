b0f769147b9a2ec352bd5884619ba7eaa0842510
Joao Gabriel <joao.gabriel156@hotmail.com>
2025-09-07 21:46:39 -0300

    feat: minimal implementation to send email transactional notification

--- notification_system/notification_workers/app.py
+++ notification_system/notification_workers/app.py
@@ -3,6 +3,7 @@ from faststream.rabbit import RabbitBroker
 
 from .schemas.message_schemas import SingleEmailNotification
 from .services.template_services import get_template
+from .services.mail_services import send_mail_single
 from .settings import settings
 
 broker = RabbitBroker(settings.BROKER_URL)
@@ -12,6 +13,14 @@ app = FastStream(broker=broker)
 @broker.subscriber('in-queue')
 async def handle_msg(message: SingleEmailNotification) -> None:
     template = await get_template(message.template_id)
-    renderized_template = template.render_body(message.keys)
+    renderized_template = template.render_body(message.body_keys)
+    renderized_subject = template.render_subject(message.subject_keys)
+    await send_mail_single(
+        message_body=renderized_template,
+        to = message.recipient,
+        from_mail="teste@meuzovo.com.br",
+        subject=renderized_subject
+    )
 
-    print(renderized_template)
+
+
