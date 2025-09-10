cc083dc3056b777b989a305ee7142f34a6052e91
Joao Gabriel <joao.gabriel156@hotmail.com>
2025-09-10 20:04:42 -0300

    fix: add tenant_id to SingleEmailNotification schema

--- notification_system/notification_workers/schemas/message_schemas.py
+++ notification_system/notification_workers/schemas/message_schemas.py
@@ -7,5 +7,6 @@ from pydantic import BaseModel, EmailStr
 class SingleEmailNotification(BaseModel):
     recipient: EmailStr
     template_id: UUID
+    tenant_id: UUID
     body_keys: dict[str, Any]
     subject_keys: dict[str, Any]
