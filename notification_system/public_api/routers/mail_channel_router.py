0ef726dc4373f29709c17f0f6cc74e06773b44a8
Joao Gabriel <joao.gabriel156@hotmail.com>
2025-09-10 20:19:47 -0300

    feat: publish event on queue

--- notification_system/public_api/routers/mail_channel_router.py
+++ notification_system/public_api/routers/mail_channel_router.py
@@ -4,8 +4,10 @@ from uuid import UUID
 from fastapi import APIRouter, Depends, HTTPException
 
 from ..security import PublicFacingKeycloakToken, RoleChecker
-from ..services.mail_services import get_template
+from ..services.mail_services import get_template, list_templates
 from ..schemas.mail_channel import SendSingleMail
+from ..broker import broker, SingleEmailNotification
+
 
 mail_channel_router = APIRouter(
     prefix= '/mail',
@@ -37,11 +39,40 @@ async def send_mail_view(user: T_send_role, send_mail_request: SendSingleMail):
             detail='Template not found',
         )
     
-    ...
+
+    if (
+        template.body_context != [*send_mail_request.body_context]
+        or
+        template.subject_context != [*send_mail_request.subject_context]
+        ):
+        raise HTTPException(
+            status_code= HTTPStatus.BAD_REQUEST,
+            detail = 'Registered template context and given request dont match'
+        )
+    
+    a = await broker.publish(
+        message=SingleEmailNotification(
+            recipient=send_mail_request.recipient,
+            template_id=send_mail_request.template_id,
+            body_keys=send_mail_request.body_context,
+            subject_keys=send_mail_request.subject_context,
+            tenant_id=user['tenant_id']
+        ),
+        queue='in-queue'
+    )
+
+
+
+@mail_channel_router.get('/{template_id}')
+async def get_template_view(template_id: UUID, user: T_get_template_role):
+    template = await get_template(template_id= template_id, tenant_id=user['tenant_id'])
+    return template
 
 
 
 @mail_channel_router.get('/')
-async def get_template_view(template_id: UUID, user: T_get_template_role):
-    template = await get_template(template_id= template_id, tenant_id=user['tenant_id'])
+async def list_template_view(user: T_get_template_role):
+    template = await list_templates(tenant_id=user['tenant_id'])
     return template
+
+
