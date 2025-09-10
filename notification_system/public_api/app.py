871c932fa8bc056aa30b4896cf5859eaad54513e
Joao Gabriel <joao.gabriel156@hotmail.com>
2025-09-10 20:06:01 -0300

    chore: enhance documentation - add api name

--- notification_system/public_api/app.py
+++ notification_system/public_api/app.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI, Depends
+from contextlib import asynccontextmanager
 
-
+from .broker import router
 from .routers.mail_channel_router import mail_channel_router
 
-app = FastAPI()
+app = FastAPI(
+    title="public api"
+)
+
+
+
+
 
 
 app.include_router(mail_channel_router)
+app.include_router(router)
